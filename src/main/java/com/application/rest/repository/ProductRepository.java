571485d4eb266aa431146c51102cf63fe4dbca07
Diego Muñoz Castro <diego.andresmc@gmail.com>
2023-12-01 19:14:31 -0300

    Capa de Persistencia

--- src/main/java/com/application/rest/repository/ProductRepository.java
+++ src/main/java/com/application/rest/repository/ProductRepository.java
@@ -4,6 +4,10 @@ import com.application.rest.entities.Product;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.math.BigDecimal;
+import java.util.List;
+
 @Repository
 public interface ProductRepository extends CrudRepository<Product, Long> {
+    List<Product> findProductByPriceBetween(BigDecimal minPrice, BigDecimal maxPrice);
 }
