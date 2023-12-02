0fe5b0e6bccc87de5593ab10aa73f83a10ac41fc
Diego Muñoz Castro <diego.andresmc@gmail.com>
2023-12-02 00:58:48 -0300

    capa servicios impl

---
+++ src/main/java/com/application/rest/service/Impl/MakerServiceImpl.java
@@ -0,0 +1,39 @@
+package com.application.rest.service.Impl;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.application.rest.entities.Maker;
+import com.application.rest.persistence.IMakerDAO;
+import com.application.rest.service.IMakerService;
+
+@Service
+public class MakerServiceImpl implements IMakerService {
+
+    @Autowired
+    private IMakerDAO makerDAO;
+
+    @Override
+    public List<Maker> findAll() {
+        return makerDAO.findAll();
+    }
+
+    @Override
+    public Optional<Maker> findById(Long id) {
+        return makerDAO.findById(id);
+    }
+
+    @Override
+    public void save(Maker maker) {
+        makerDAO.save(maker);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        makerDAO.deleteById(id);
+    }
+
+}
