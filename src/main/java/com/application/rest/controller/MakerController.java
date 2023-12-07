fcfa0422eb679d19e2ad9edbeea1c91685932e8f
Diego Muñoz Castro <diego.andresmc@gmail.com>
2023-12-07 01:10:37 -0300

    CRUD Controller

--- src/main/java/com/application/rest/controller/MakerController.java
+++ src/main/java/com/application/rest/controller/MakerController.java
@@ -5,11 +5,11 @@ import com.application.rest.entities.Maker;
 import com.application.rest.service.IMakerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.List;
 import java.util.Optional;
 
 @RestController
@@ -35,4 +35,52 @@ public class MakerController {
         return ResponseEntity.notFound().build();
     }
 
+    @GetMapping("/findAll")
+    public ResponseEntity<?> findAll() {
+        List<MakerDTO> makerList = makerService.findAll()
+                .stream()
+                .map(maker -> MakerDTO.builder()
+                        .id(maker.getId())
+                       .name(maker.getName())
+                       .productList(maker.getProductList())
+                        .build())
+               .toList();
+        return ResponseEntity.ok(makerList);
+    }
+
+    @PostMapping("/save")
+    public ResponseEntity<?> save(@RequestBody MakerDTO makerDTO) throws URISyntaxException {
+
+        if(makerDTO.getName().isBlank()){
+            return ResponseEntity.badRequest().build();
+        }
+
+        makerService.save(Maker.builder()
+                .name(makerDTO.getName())
+                .build());
+
+        return ResponseEntity.created(new URI("api/maker/save")).build();
+    }
+
+    @PutMapping("/update/{id}")
+    public ResponseEntity<?> updateMaker(@PathVariable Long id, @RequestBody MakerDTO makerDTO){
+        Optional<Maker> makerOptional = makerService.findById(id);
+
+        if (makerOptional.isPresent()) {
+            Maker maker = makerOptional.get();
+            maker.setName(makerDTO.getName());
+            makerService.save(maker);
+            return ResponseEntity.ok("Registro Actualizado");
+        }
+        return ResponseEntity.notFound().build();
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<?> deleteById(@PathVariable Long id){
+        if(id != null){
+            makerService.deleteById(id);
+            return ResponseEntity.ok("Registro Eliminado");
+        }
+        return ResponseEntity.badRequest().build();
+    }
 }
